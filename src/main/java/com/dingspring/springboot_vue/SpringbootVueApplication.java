436824c47dc91a71f28a2f1aa1a0b6aa2ae883d4
dingwen-star <771268289@qq.com>
2021-03-27 17:03:57 +0800

    2-2 修改启动文案

--- src/main/java/com/dingspring/springboot_vue/SpringbootVueApplication.java
+++ src/main/java/com/dingspring/springboot_vue/SpringbootVueApplication.java
@@ -1,13 +1,19 @@
 package com.dingspring.springboot_vue;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class SpringbootVueApplication {
-
+    private static final Logger log = LoggerFactory.getLogger(SpringbootVueApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootVueApplication.class, args);
+        SpringApplication app = new SpringApplication(SpringbootVueApplication.class);
+        ConfigurableEnvironment env = app.run(args).getEnvironment();
+        log.info("启动成功");
+        log.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
