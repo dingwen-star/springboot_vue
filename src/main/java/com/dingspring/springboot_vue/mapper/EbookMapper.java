2afff189eed4b9115e4b29716e5ea35f8addd47e
dingwen-star <771268289@qq.com>
2021-03-30 23:21:09 +0800

    生成ebook表信息

---
+++ src/main/java/com/dingspring/springboot_vue/mapper/EbookMapper.java
@@ -0,0 +1,30 @@
+package com.dingspring.springboot_vue.mapper;
+
+import com.dingspring.springboot_vue.domain.Ebook;
+import com.dingspring.springboot_vue.domain.EbookExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface EbookMapper {
+    long countByExample(EbookExample example);
+
+    int deleteByExample(EbookExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Ebook record);
+
+    int insertSelective(Ebook record);
+
+    List<Ebook> selectByExample(EbookExample example);
+
+    Ebook selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByExample(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByPrimaryKeySelective(Ebook record);
+
+    int updateByPrimaryKey(Ebook record);
+}
